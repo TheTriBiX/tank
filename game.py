dbdc71ac01d86bba6e3cc6a3ee363580e189c595
TheTriBiX <thetribixtv@gmail.com>
2021-01-19 11:31:54 +0300

    изменен экран смерти

--- game.py
+++ game.py
@@ -20,21 +20,18 @@ def terminate():
 
 
 def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+    intro_text = ['нажмите любую клавишу']
 
-    fon = pygame.transform.scale(load_image('Без названия.jpg'), (width, height))
+    fon = pygame.transform.scale(load_image('game_over.png'), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
         string_rendered = font.render(line, 1, pygame.Color('black'))
         intro_rect = string_rendered.get_rect()
-        text_coord += 10
+        text_coord = 650
         intro_rect.top = text_coord
-        intro_rect.x = 10
+        intro_rect.x = 100
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
 
